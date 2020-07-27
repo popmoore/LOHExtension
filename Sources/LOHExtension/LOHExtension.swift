db1293f24f58134a7c06b9250f7d2a13db5b59cc
LockedOutofHeaven <303762442@qq.com>
2020-07-28 03:17:58 +0800

    feat: make parseQuery accessible

--- Sources/LOHExtension/LOHExtension.swift
+++ Sources/LOHExtension/LOHExtension.swift
@@ -6,7 +6,7 @@ extension URLComponents {
     /// If `query` is nil, the result is nil. If `query` is an empty string, it returns `[]`
     ///
     /// - Parameter query: The query component string
-    static func parseQuery(_ query: String?) -> [URLQueryItem]? {
+    public static func parseQuery(_ query: String?) -> [URLQueryItem]? {
         var urlComponents = URLComponents()
         urlComponents.percentEncodedQuery = query
         return urlComponents.queryItems
